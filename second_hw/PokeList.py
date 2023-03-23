cf96ea63381957c7fe63aea0d22db545c8fc5b3f
DanieleAndrean <daniele.andrean@outlook.com>
2023-03-23 18:12:28 +0100

    Assignment2 folder+ new poke/moves

---
+++ second_hw/PokeList.py
@@ -0,0 +1,42 @@
+PkList=[  {"national_pokedex_number": 1,
+                 "name": "bulbasaur",
+                 "types": ["grass", "poison"],
+                 "baseStats": {"hp": 45, "attack": 49, "defense": 49, "speed": 45, "special": 65},
+                 "moves" : ["tackle", "razor leaf"]},
+
+          {"national_pokedex_number": 4,
+                 "name": "charmander",
+                 "types": ["fire"],
+                 "baseStats": {"hp": 39, "attack": 52, "defense": 43, "speed": 65, "special": 50},
+                 "moves" : ["tackle", "ember"]},
+
+          {"national_pokedex_number": 7,
+                 "name": "squirtle",
+                 "types": ["water"],
+                 "baseStats": {"hp": 44, "attack": 48, "defense": 65, "speed": 43, "special": 50},
+                 "moves" : ["tackle", "water gun"]},
+
+          {"national_pokedex_number": 10,
+               "name": "caterpie",
+               "types": ["bug"],
+               "baseStats": {"hp": 45, "attack": 30, "defense": 35, "speed": 45, "special": 20},
+               "moves": ["twineedle"]},
+
+          {"national_pokedex_number": 16,
+               "name": "pidgey",
+               "types": ["normal", "flying"],
+               "baseStats": {"hp": 40, "attack": 45, "defense": 40, "speed": 56, "special": 35},
+               "moves" : ["tackle", "peck"]},
+
+          {"national_pokedex_number": 19,
+               "name": "rattata",
+               "types": ["normal"],
+               "baseStats": {"hp": 30, "attack": 56, "defense": 35, "speed": 72, "special": 25},
+               "moves" : ["tackle"]},
+]
+
+
+
+          
+               
+          
